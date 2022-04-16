fee2fd52c74946aacbbf29830ca4f582b7e1410e
Godm0de <Godm0de@github.io>
2022-04-16 16:27:23 +0200

    Modify search bar styles

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,4 +1,4 @@
 
-export const ProfileCard = () => {
-
+export const ProfileCard = ({user}) => {
+    
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,1 +1,18 @@
-export const SearchBar = ({ onSearch }) => {}
+import { useState } from "react";
+import "./SearchBar.css";
+
+export const SearchBar = ({ onSearch }) => {
+    const [value, setValue] = useState('');
+
+    return (
+        <div className="search-bar">
+            <input
+                className="search-bar__input"
+                type="text"
+                placeholder="Search Github username..."
+                onChange={e => setValue(e.target.value)}
+            />
+            <button className="search-bar__button">Search</button>
+        </div>
+    )
+}
