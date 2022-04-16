02a2a400ae7884d9430d8230c605ca80e720dacd
Godm0de <Godm0de@github.io>
2022-04-16 16:07:01 +0200

    Create basic component layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react'
+import { getUserInfo } from './services/user';
+import { ProfileCard } from './components/ProfileCard';
+import { Toolbar } from './components/Toolbar';
+import { SearchBar } from './components/SearchBar';
 import './App.css'
 
 function App() {
-
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    getUserInfo('Godm0de').then(setUser);
+  }, [])
   return (
-    <h1>Hello World</h1>
+    <main className='container'>
+      <Toolbar title="devfinder" />
+      <SearchBar />
+      <ProfileCard />
+    </main>
   )
 }
 
